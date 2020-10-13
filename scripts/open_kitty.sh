7af91d5e976dc2280e1ce33d553d52129a7399ac
jasonIV <thuhtetaung.d2@gmail.com>
2020-10-13 13:20:46 +0630

    updated

--- scripts/open_kitty.sh
+++ scripts/open_kitty.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# Detects if iTerm2 is running
+# Detects if kitty is running
 if ! pgrep -f "kitty" > /dev/null; then
     open -a "/Applications/kitty.app"
 else
     # Create a new window
     if ! osascript -e 'tell application "kitty" to create window with default profile' > /dev/null; then
-        # Get pids for any app with "iTerm" and kill
+        # Get pids for any app with "kitty" and kill
         for i in $(pgrep -f "kitty"); do kill -15 "$i"; done
         open  -a "/Applications/kitty.app"
     fi
